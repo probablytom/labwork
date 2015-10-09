75529e2e7b93cf48a254014d1d405bfda950560e
Tom Wallis <tom@tom-wallis.com>
2015-10-09 10:10:22 +0100

    Began AI lab 1 work

---
+++ AI/Labs/01/lab_implementation.py
@@ -0,0 +1,29 @@
+# Determine the Sampling Rate (file samples are 300ms speech)
+#   Note: there are 2400 samples.
+
+s = []
+
+with open("lab.dat") as labdat:
+    for line in labdat.readlines():
+        if line != "\n": s.append(line.rstrip())
+
+sampling_rate = len(s)/0.3 # 0.3s...
+
+
+# Apply the ideal delay operator with delay 5ms, 10ms, and 15ms
+y = [[], [], []]  # For the ideal delay operators at 5, 10 and 15.
+
+def ideal_delay_by_5ms(input_signal):
+    # ideal delay y[n] = s[n-n0]
+    n0 = len(input_signal) / (5 * samp_rate)
+
+
+# Apply the moving average with k1=k2=5, 10, and 15ms
+
+# Convolve the signal with a window of length 10ms
+
+# Extract the shirt0term energy signal from the signal in labrotory.dat
+
+# Plot the orginal signal, the energy signal, the magnitude signal and the .ZCR signal as a function of time
+
+
